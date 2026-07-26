c234bc9a2f4ed95148b9499241512cac747ab0a1
Pietro Acampora <pietrolacampora@gmail.com>
2026-07-26 13:55:38 +0200

    Add activity list mouse controller interface

---
+++ src/gui/ActivityListMouseController.h
@@ -0,0 +1,45 @@
+#ifndef ACTIVITYLISTMOUSECONTROLLER_H
+#define ACTIVITYLISTMOUSECONTROLLER_H
+
+#include <QObject>
+#include <QPointer>
+
+class QAction;
+class QEvent;
+class QListWidget;
+class QListWidgetItem;
+class QMainWindow;
+class QPoint;
+class QPushButton;
+class QStackedWidget;
+class QWidget;
+
+class ActivityListMouseController final : public QObject
+{
+public:
+    explicit ActivityListMouseController(QObject* parent = nullptr);
+
+protected:
+    bool eventFilter(QObject* watched, QEvent* event) override;
+
+private:
+    void configureWindow(QMainWindow* window);
+    void editItem(QListWidgetItem* item);
+    void showContextMenu(const QPoint& position);
+    bool workflowActive() const;
+
+    QPushButton* buttonWithText(QMainWindow* window, const QString& text) const;
+    QPushButton* buttonStartingWith(QMainWindow* window, const QString& prefix) const;
+    QAction* actionStartingWith(QMainWindow* window, const QString& prefix) const;
+
+    QPointer<QMainWindow> m_window;
+    QPointer<QListWidget> m_activityList;
+    QPointer<QStackedWidget> m_workspaceStack;
+    QPointer<QWidget> m_detailPage;
+    QPointer<QPushButton> m_editButton;
+    QPointer<QPushButton> m_toggleButton;
+    QPointer<QPushButton> m_deleteButton;
+    QPointer<QAction> m_saveAsTemplateAction;
+};
+
+#endif
