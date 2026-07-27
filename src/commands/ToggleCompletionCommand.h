0fbbd1dbd6343dbc7f5e3b8024c45c56318d6d20
Pietro Acampora <pietrolacampora@gmail.com>
2026-07-27 12:07:54 +0200

    Add contextual labels to completion command

--- src/commands/ToggleCompletionCommand.h
+++ src/commands/ToggleCompletionCommand.h
@@ -12,8 +12,8 @@ class ActivityManager;
 /*
  * Command concreta per cambiare lo stato completato/attivo di un'attività.
  *
- * Ho scelto di salvare lo stato precedente e quello nuovo perché l'undo deve
- * ripristinare esattamente la situazione iniziale, non limitarsi a fare toggle.
+ * Lo stato iniziale viene catturato una sola volta. Undo ripristina quel
+ * valore esatto e redo applica sempre il valore di destinazione originario.
  */
 class ToggleCompletionCommand : public Command
 {
@@ -24,6 +24,8 @@ public:
     bool execute() override;
     bool undo() override;
     QString description() const override;
+    QString undoDescription() const override;
+    QString redoDescription() const override;
 
     QString activityId() const;
 
@@ -35,6 +37,7 @@ private:
 
     bool m_previousCompleted = false;
     bool m_newCompleted = false;
+    bool m_stateCaptured = false;
     bool m_hasBeenExecuted = false;
 };
 
